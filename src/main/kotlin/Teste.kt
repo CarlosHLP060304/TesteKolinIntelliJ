1eff7b35f2cb7c2ee6dae50359e25c3455de8db9
Carlos-Henrique-Lins-Ponchirolli <ch.ponchirolli@gmail.com>
2024-02-01 01:48:05 -0300

    Initial commit

---
+++ src/main/kotlin/Teste.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    print("Digite o nome do novo usuário do Kotlin: ")
+    var name = sc.nextLine()
+    print("Bem vindo ao mundo Kotlin $name")
+
+}
